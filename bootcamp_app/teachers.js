d34dbe9e97ef577439a191f746c118abdde95d13
Jovita <jovitaspais@gmail.com>
2022-01-06 19:24:55 +0000

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -12,15 +12,16 @@ const value = process.argv.slice(2);
 // const limit = process.argv[3];
 
 pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort
-FROM students
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE $1
-LIMIT $2;
-`, value)
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
   .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
     });
-  })
-  .catch(err => console.error('query error', err.stack));
+  });
